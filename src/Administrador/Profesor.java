90aa9cddf162d85739de4cf988348f04f854b85d
Personal <mgtubac@gmail.com>
2023-06-15 03:32:35 -0600

    Firs

---
+++ src/Administrador/Profesor.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Administrador;
+
+/**
+ *
+ * @author DELL
+ */
+public class Profesor {
+
+    public String codigo;
+    public String nombre;
+    public String apellido;
+    public String correo;
+    public String contrasenia;
+    public String genero;
+
+    public Profesor(String codigo, String nombre, String apellido,
+            String correo, String contrasenia, String genero) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.contrasenia = contrasenia;
+        this.genero = genero;
+    }
+
+}
---
+++ src/Profesores/Alumnos.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Profesores;
+
+/**
+ *
+ * @author DELL
+ */
+public class Alumnos {
+
+    public String codigo;
+    public String nombre;
+    public String apellido;
+    public String correo;
+    public String genero;
+
+    public Alumnos(String codigo, String nombre, String apellido,
+            String correo, String genero) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.genero = genero;
+    }
+
+}
